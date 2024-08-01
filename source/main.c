4666e609e11b3b842d7373f03b21d82d262c29f7
Nicolas Magee Rosado <nicolasmageerosado@gmail.com>
2024-08-01 14:22:49 +0200

    send_buffer_idx starts at -1 to avoid overlap

--- source/main.c
+++ source/main.c
@@ -11,7 +11,7 @@
 #define BUFFER_SIZE 256
 
 char send_buffer[BUFFER_SIZE] = {0xff};
-int send_buffer_idx = 0;
+int send_buffer_idx = -1;
 
 
 void led_toggle(void);
@@ -43,12 +43,17 @@ int main(void)
 }
 
 void send_keycodes(char* keys) {
-    if (send_buffer_idx != 0) return;
+    // If we are still sending previous keycodes
+    if (send_buffer_idx != -1) return;
+    send_buffer_idx++;
 
+    // Copy over to global buffer
     for(int i = 0; i < BUFFER_SIZE; i++) {
         if (keys[i] == 0xff) break;
         send_buffer[i] = keys[i];
     }
+
+    // Make an initial empty report to start the callback chain
     uint8_t keycode[6] = {0};
     tud_hid_keyboard_report(1, 0, keycode);
 }
@@ -69,7 +74,7 @@ void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint16_
         send_buffer[send_buffer_idx] = 0xff;
         send_buffer_idx++;
     } else {
-        send_buffer_idx = 0;
+        send_buffer_idx = -1;
     }
 }
 
